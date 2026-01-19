d90a1342e4a3593d5cb3cd02fc620d14257152d8
kristinalazareva <kristinkaell@mail.ru>
2026-01-19 12:15:48 +0300

    Тестирование Класса Сложения Векторов [#505262]

---
+++ src/test/java/ru/job4j/condition/VectorTest.java
@@ -0,0 +1,32 @@
+package ru.job4j.condition;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class VectorTest {
+
+    @Test
+    void whenWektors12And34ThenResult46() {
+        int x1 = 1, y1 = 2, x2 = 3, y2 = 4;
+        String expected = "(4, 6)";
+        String output = Vector.addVectors(x1, y1, x2, y2);
+        assertThat(output).isEqualTo(expected);
+    }
+
+    @Test
+    void whenWektorsMinus12And34Result22() {
+        int x1 = -1, y1 = -2, x2 = 3, y2 = 4;
+        String expected = "(2, 2)";
+        String output = Vector.addVectors(x1, y1, x2, y2);
+        assertThat(output).isEqualTo(expected);
+    }
+
+    @Test
+    void whenWektorsMinus12AndMinus34ResultMinus46() {
+        int x1 = -1, y1 = -2, x2 = -3, y2 = -4;
+        String expected = "(-4, -6)";
+        String output = Vector.addVectors(x1, y1, x2, y2);
+        assertThat(output).isEqualTo(expected);
+    }
+}
