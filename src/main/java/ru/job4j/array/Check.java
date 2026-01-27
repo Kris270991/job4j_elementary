cfe7dd0a482dd9a9c6e2b8dd05fd2643c5beb9fd
kristinalazareva <kristinkaell@mail.ru>
2026-01-27 13:44:15 +0300

    Массив заполнен true или false [#505232 #557456]

--- src/main/java/ru/job4j/array/Check.java
+++ src/main/java/ru/job4j/array/Check.java
@@ -3,9 +3,8 @@ package ru.job4j.array;
 public class Check {
     public static boolean mono(boolean[] data) {
         boolean result = true;
-        for (boolean element : data) {
-            boolean temp = data[0];
-            if (element != temp) {
+        for (int i = 1; i < data.length; i++) {
+            if (data[0] != data[i]) {
                 result = false;
                 break;
             }
