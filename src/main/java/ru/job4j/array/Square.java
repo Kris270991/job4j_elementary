c9586a6866472acb8931d8c49bf9fdac3c8ca716
kristinalazareva <kristinkaell@mail.ru>
2026-02-11 15:36:21 +0300

    1. Чтение кода и базовые ошибки [#40205]

--- src/main/java/ru/job4j/array/Square.java
+++ src/main/java/ru/job4j/array/Square.java
@@ -6,7 +6,6 @@ public class Square {
         for (int i = 0; i < bound; i++) {
             result[i] = i * i;
         }
-        /* заполнить массив через цикл элементами от 0 до bound , возведенными в квадрат */
         return result;
     }
 
--- src/test/java/ru/job4j/array/GreedyAlgorithmTest.java
+++ src/test/java/ru/job4j/array/GreedyAlgorithmTest.java
@@ -3,12 +3,12 @@ package ru.job4j.array;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class MachineTest {
+public class GreedyAlgorithmTest {
     @Test
     public void whenMoneyEqualsPrice() {
         int money = 100;
         int price = 100;
-        int[] result = Machine.change(money, price);
+        int[] result = GreedyAlgorithm.change(money, price);
         int[] expected = {};
         assertThat(result).containsExactly(expected);
     }
@@ -17,7 +17,7 @@ public class MachineTest {
     public void whenMoney50Price35() {
         int money = 50;
         int price = 35;
-        int[] result = Machine.change(money, price);
+        int[] result = GreedyAlgorithm.change(money, price);
         int[] expected = {10, 5};
         assertThat(result).containsExactly(expected);
     }
@@ -26,7 +26,7 @@ public class MachineTest {
     public void whenMoney50Price21() {
         int money = 50;
         int price = 21;
-        int[] result = Machine.change(money, price);
+        int[] result = GreedyAlgorithm.change(money, price);
         int[] expected = {10, 10, 5, 2, 2};
         assertThat(result).containsExactly(expected);
     }
@@ -35,7 +35,7 @@ public class MachineTest {
     public void whenMoney50Price32() {
         int money = 50;
         int price = 32;
-        int[] result = Machine.change(money, price);
+        int[] result = GreedyAlgorithm.change(money, price);
         int[] expected = {10, 5, 2, 1};
         assertThat(result).containsExactly(expected);
     }
